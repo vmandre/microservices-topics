b26d8665512d423e1ce9bdbe28908246cee60a0f
vmandre <vmandre@gmail.com>
2023-02-17 00:39:33 +1300

    feat: Create Person resource with produces version param

--- src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
+++ src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
@@ -36,4 +36,14 @@ public class PersonVersioningController {
     public PersonV2 headerV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1() {
+        return new PersonV1(("Bob Charlie"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
