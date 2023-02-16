f1b1ddfb7c57f794c79a3e66438f0be5130a5fde
vmandre <vmandre@gmail.com>
2023-02-17 00:50:01 +1300

    feat: Add documentation for the resources created

--- src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
+++ src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+    // URI versioning - It pollutes the URI as we're creating new URIs
+
     @GetMapping("/v1/person")
     public PersonV1 personV1() {
         return new PersonV1(("Bob Charlie"));
@@ -16,6 +18,9 @@ public class PersonVersioningController {
     public PersonV2 personV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+
+    // Request parameter versioning - It pollutes the URI as we're creating new URI parameters
 
     @GetMapping(value = "/person/param", params = "version=1")
     public PersonV1 paramV1() {
@@ -27,6 +32,10 @@ public class PersonVersioningController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+
+    // Headers versioning - It doesn't pollute the URI as it is the same
+    // for both and just change the header parameter
+
     @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
     public PersonV1 headerV1() {
         return new PersonV1(("Bob Charlie"));
@@ -36,6 +45,10 @@ public class PersonVersioningController {
     public PersonV2 headerV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+    
+    // Media type versioning (a.k.a "content negotiation" or "accept header")
+    // It doesn't pollute the URI as it is the same
+    // for both and just change the header parameter
 
     @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
     public PersonV1 producesV1() {
