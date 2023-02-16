a81a24f0a2f797107f7c6eeca7f3a7aad1f19747
vmandre <vmandre@gmail.com>
2023-02-17 00:30:35 +1300

    feat: Create Person resource with param version

--- src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
+++ src/main/java/com/vmandre/microservices/versioning/PersonVersioningController.java
@@ -16,4 +16,14 @@ public class PersonVersioningController {
     public PersonV2 personV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 paramV1() {
+        return new PersonV1(("Bob Charlie"));
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 paramV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
