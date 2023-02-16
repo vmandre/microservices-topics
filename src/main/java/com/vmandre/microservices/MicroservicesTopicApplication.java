05abe7b1744dc4af7078e7debb351631655078b1
Vanderlei Matos Andre <vanderlei.matos.andre@westpac.co.nz>
2023-02-16 17:55:03 +1300

    Initial commit

---
+++ src/main/java/com/vmandre/microservices/MicroservicesTopicApplication.java
@@ -0,0 +1,13 @@
+package com.vmandre.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTopicApplication.class, args);
+	}
+
+}
